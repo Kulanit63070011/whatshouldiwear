dae640332f0ad5f6968be05cc68b985beaf9967d
Kulanit63070011 <73680667+Kulanit63070011@users.noreply.github.com>
2023-04-11 17:26:07 +0700

    commit at kmitl central 2 ..

--- frontend/screens/AllOptionsScreen.jsx
+++ frontend/screens/AllOptionsScreen.jsx
@@ -1,20 +1,40 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 
 const AllOptionsScreen = () => {
   const route = useRoute();
   const { selectedOptions } = route.params;
-  
+  console.log(selectedOptions);
 
   return (
-    <View>
+    <View style={styles.container}>
+      <Text style={styles.title}>Selected Options:</Text>
       {selectedOptions.map((option, index) => (
-        <Text key={index}>{option}</Text>
+        <Text key={index} style={styles.option}>
+          {option}
+        </Text>
       ))}
     </View>
-    
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  option: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+});
 
 export default AllOptionsScreen;
